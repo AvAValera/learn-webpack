7a4d90cdf740be0ff6522dd2a4ee97b4624dbc04
AvAValera <avavarenik07@gmail.com>
2022-01-25 10:19:37 +0200

    start project

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports ={
+    entry: {
+        main: './src/index.js',
+    },
+    output:{
+        path : path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+
+}
