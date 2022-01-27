447e00c7ee021c653d6b198b42fc07dc377cdff1
AvAValera <avavarenik07@gmail.com>
2022-01-27 15:18:14 +0200

    edit code build in folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Main from './Main'
-import logo from './img/main/webpack.png'
 export default function App() {
     return (
         <div>
             <span>Hello react</span>
                 <Main />
                 
-                <img src={logo} alt="webpack" />
+                <img src='/img/main/webpack.png' alt="webpack" />
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,63 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin")
-const webpack = require("webpack");
 module.exports = {
+    resolve: {
+        alias: {
+            images: path.resolve(__dirname, 'src/img/'),
+        },
+    },
     entry: {
         main: [ "./src/index.jsx"],
     },
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "static/js/bundle.js",
+        path: path.resolve(__dirname, "build"),
+        filename: "static/js/main.[hash].js",
     },
     mode: "development",
     devServer: {
         hot: true,
         port: 3000,
         host: "localhost",
+        static: './src',
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./public/index.html",
         }),
-        new CleanWebpackPlugin(),
-        
+        new MiniCssExtractPlugin({
+            filename: 'static/css/main.[contenthash].css',
+        }),
+        // new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'public/img'),
+                    to: path.resolve(__dirname, 'build/img')
+                }
+            ]
+        }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, 
+                    "css-loader"],
             },
             {
                 test: /\.s[ac]ss$/i,
-                use:["style-loader", "css-loader", "sass-loader"]
+                use:[MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
             },
             {
                 test: /\.(png|jpg|svg|gif)$/,
                 use: {
                     loader: "file-loader",
                     options: {
-                        name: '/img/[path]/[name].[ext]',
+                        name: 'img/[path]/[name].[ext]',
+                        
                     }
                 }
                 
